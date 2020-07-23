cff74a36a9f4b3a941b9d14fa0d987b724192308
noner <dobr-andr@mail.ru>
2020-07-23 14:42:44 +0600

    update models

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+STATUS_CHOICES = [
+    ('new', 'новая'),
+    ('in_progress', 'В процессе'),
+    ('done', 'Сделано')
+]
+
+
+
+class Article(models.Model):
+
+    description = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Описание')
+
+    status = models.CharField(max_length=15, choices=STATUS_CHOICES, default='new', verbose_name='статус')
+
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+
+    def __str__(self):
+        return "{}. {}".format(self.pk, self.description)
